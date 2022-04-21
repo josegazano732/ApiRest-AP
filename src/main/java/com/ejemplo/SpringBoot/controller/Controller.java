2ad3606adf8eac3748703515d7546df8a5121ff7
josegazano732 <josegazano7@outlook.com>
2022-04-21 00:29:39 -0300

    Se agrego CrossOrigins JLG

--- src/main/java/com/ejemplo/SpringBoot/controller/Controller.java
+++ src/main/java/com/ejemplo/SpringBoot/controller/Controller.java
@@ -9,6 +9,7 @@ import com.ejemplo.SpringBoot.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/personas")
 public class Controller {
